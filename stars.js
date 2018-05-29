898f8b724c0a9763120e2b891a4dc4c163709c1d
Erik Withak <ErikAanestad@gmail.com>
2018-05-28 22:42:56 -0700

    added message and fixed timing/styles

--- stars.js
+++ stars.js
@@ -1,6 +1,6 @@
-setTimeout(() => {
-    setStars();
-}, 20000);
+setTimeout(() => setStars(), 20000);
+
+setTimeout(() => message(), 27000);
 
 const starCount = 50;
 let stars = 0;
@@ -22,4 +22,18 @@ function setStars() {
     else {
         setTimeout(() => setStars(), (Math.random() * 1000));
     }
+}
+
+function message() {
+    const message = document.createElement('span');
+    message.id = 'message';
+    message.textContent = 'Nighty Night';
+    message.style.animation = 'message-fade-in 3s forwards';
+    document.getElementsByTagName('body')[0].appendChild(message);
+    setTimeout(() => messageGlow(), 3000);
+}
+
+function messageGlow() {
+    const message = document.getElementById('message');
+    message.style.animation = 'message-glow 3s alternate infinite';
 }
